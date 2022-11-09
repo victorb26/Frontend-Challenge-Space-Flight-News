3e5c2968f11d969726a51db065c00f738c367b98
Victor Barros <victorjuda49@gmail.com>
2022-11-09 10:31:16 -0300

    refact: rebuilding 'index.css'

--- space-flight/src/components/Buttons/ButtonCarregar.jsx
+++ space-flight/src/components/Buttons/ButtonCarregar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import "./ButtonCarregar.module.css";
+import styles from "./ButtonCarregar.module.css";
 
 export default function ButtonCarregar() {
   return(
-    <div className="btnCarregar">
+    <div className={styles.btnCarregar}>
         <button>Carregar Mais</button>
     </div> 
 )
